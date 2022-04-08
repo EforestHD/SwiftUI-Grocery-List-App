b9c6e28b5b457af48d5993e8cd5b650e635d2aee
Ewald <57798772+EforestHD@users.noreply.github.com>
2022-04-08 13:10:51 +0200

    init

---
+++ SwiftUI-Grocery/SwiftUI-Grocery/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-Grocery
+//
+//  Created by Ewald Kerner on 08.04.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Please feel free to contact me on Twitter @EwaldKerner")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI-Grocery/SwiftUI-Grocery/SwiftUI_GroceryApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_GroceryApp.swift
+//  SwiftUI-Grocery
+//
+//  Created by Ewald Kerner on 08.04.22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_GroceryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
